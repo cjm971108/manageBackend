9aa4bad5c0d695f55757c13e4fa798e99a3f0ec2
cjm <cjm@qq.com>
2019-12-20 18:17:19 +0800

    完成登录功能 #3

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<!-- 后台管理系统首页 -->
+<template>
+  <div class>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      window.sessionStorage.clear() // 清空sessionStorage
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+<style lang='less' scoped>
+</style>
